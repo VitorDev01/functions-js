b9a5eda09b1c5d847c1975d2d821ccdb77fc8de2
Vitor Oliveira <83097345+VitorDev01@users.noreply.github.com>
2022-02-05 12:57:47 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,33 @@
+    
+     //funções
+            //nome//
+    function parimpar(n) {
+      if (n%2==0) {
+        return 'par'
+      } else {
+        return 'impar'
+      }
+    }
+   // chamando a função e coclocando na variavel //
+    var res = parimpar(4)
+    console.log(res) // não se põe aspas em váriaveis//
+    
+   // fazendo direto//
+                      //4 é o paremetro//
+   console.log(parimpar(4))
+    
+    
+    function soma(n1, n2) {
+       return n1 + n2
+    }
+    console.log(soma(100, 1000))
+    
+    //Array
+    let num = [5 , 8, 4]
+    
+  
+    //objetos {}     //nome      sexo       peso 
+    var amigo = { nome:'Jaum',sexo:'M' ,peso:'75.5'    , engordar(p){} }
+           //função//
+     
+     
